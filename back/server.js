c24e3d8455b3a8b4781abfde9cd03a113284da38
roshani610 <shahroshani.v@gmail.com>
2019-10-10 14:38:14 +0530

    changes

--- back/server.js
+++ back/server.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const body_parser=require('body-parser');
+require('dotenv').config();
 const app=express();
 
+let PORT=process.env.PORT || 4400;
 app.use(body_parser.urlencoded({extended:true}));
-app.listen(3000,function(){
-    console.log("app listening to 3000");
+app.listen(PORT,function(){
+    console.log("app listening to "+PORT);
 });
 app.get("/",function(re,resp){
     resp.status(200).send("Hello world");
